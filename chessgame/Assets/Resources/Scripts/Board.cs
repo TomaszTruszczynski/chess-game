2cffee761adc447e217906e34b65de65efa789d1
OskarPodgorski <oskar.podgorski.97@gmail.com>
2025-10-07 23:03:50 +0200

    created board grid using grid component

---
+++ chessgame/Assets/Resources/Scripts/Board.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+    public GameObject tilePrefab;
+    public Transform tilesRoot;
+
+    Grid boardGrid;
+
+    void Start()
+    {
+        boardGrid = GetComponent<Grid>();
+
+        for (int x = 0; x < 8; x++)
+        {
+            for (int y = 0; y < 8; y++)
+            {
+                bool isWhite = (x + y) % 2 == 0;
+
+                Vector3 worldPosition = boardGrid.GetCellCenterWorld(new Vector3Int(x - 4, y - 4, 0));
+
+                GameObject tile = Instantiate(tilePrefab, worldPosition, Quaternion.identity, tilesRoot);
+                tile.name = $"Tile_{x}_{y}";
+
+                if (!isWhite)
+                {
+                    if (tile.TryGetComponent(out SpriteRenderer sprite))
+                    {
+                        sprite.color = Color.black;
+                    }
+                }
+            }
+        }
+    }
+}
