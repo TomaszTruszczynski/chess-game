f9903025cbe722f45cea083dffbb0b84fc6d0250
TomaszTruszczynski <tomtru97@gmail.com>
2025-10-20 02:33:59 +0200

    added black pawns

--- chessgame/Assets/Resources/Scripts/Pawns.cs
+++ chessgame/Assets/Resources/Scripts/Pawns.cs
@@ -11,13 +11,33 @@ public class Pawns : MonoBehaviour
     {
         for (int x = 1; x < 9; x++)
         {
-            Vector3 worldPosition = board.GetWorldPositionByIndex(new Vector2Int(x, 2));
+            GameObject goWhite = GeneratePawn(new Vector2Int(x, 2), false);
+            GameObject goBlack = GeneratePawn(new Vector2Int(x, 7), true);
 
-            GameObject go = Instantiate(pawnPrefab, worldPosition, Quaternion.identity, transform);
-
-            pawnsList.Add(go);
+            pawnsList.Add(goWhite);
+            pawnsList.Add(goBlack);
         }
     }
+
+    private GameObject GeneratePawn(Vector2Int index, bool isBlack)
+    {
+        Vector3 worldPosition = board.GetWorldPositionByIndex(index);
+
+        GameObject go = Instantiate(pawnPrefab, worldPosition, Quaternion.identity, transform);
+
+        if (isBlack)
+        {
+            Pawn pawn = go.GetComponent<Pawn>();
+            pawn.isBlack = true;
+
+            SpriteRenderer sprite = go.GetComponent<SpriteRenderer>();
+            sprite.color = Color.blueViolet;
+        }
+
+        return go;
+    }
+
+
     public void ResetGame()
     {
         foreach (GameObject pawn in pawnsList)
